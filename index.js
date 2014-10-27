17db5297448a5426ece70209e76ba93d10cb1399
Erika Weiler <erika.weiler@students.olin.edu>
2014-10-27 11:07:32 -0400

    index.html

---
+++ index.js
@@ -0,0 +1,5 @@
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.min.js"></script>
+<script src="data.js"></script>
+
+$(document).ready(function(){});
